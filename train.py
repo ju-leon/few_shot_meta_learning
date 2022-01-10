84657749f6ae7b3cb1d289642857ec20a2b07147
Luis Augenstein <luis.augenstein@web.de>
2022-01-10 20:33:58 +0100

    sorted the cli options

--- train.py
+++ train.py
@@ -13,46 +13,11 @@ from few_shot_meta_learning.Benchmark import Benchmark
 
 def main():
     parser = argparse.ArgumentParser(description='Setup variables')
-
-    parser.add_argument("--resume_epoch", default=0,
-                        help='0 means fresh training. >0 means training continues from a corresponding stored model.')
-
-    parser.add_argument("--network_architecture", default="FcNet")
-    parser.add_argument("--logdir", default=".",
-                        help='default location to store the saved_models directory')
-
+    # dataset options
+    parser.add_argument("--benchmark", default='SinusoidAffine1D',
+                        help='possible values are Sinusoid1D, Affine1D, Quadratic1D, SinusoidAffine1D')
     parser.add_argument("--num_ways", default=1, type=int,
                         help='d_y dimension of targets')
-    parser.add_argument("--k_shot", default=8, type=int,
-                        help='number of datapoints in the context set')
-    parser.add_argument("--v_shot", default=10, type=int)
-    parser.add_argument("--num_models", default=10, type=int,
-                        help='number of models (phi) we sample from the posterior in the end for evaluation. irrelevant for maml')
-    parser.add_argument("--KL_weight", default=1e-5, type=float)
-
-    parser.add_argument("--inner_lr", default=0.01, type=float)
-    parser.add_argument("--num_inner_updates", default=5,
-                        type=int, help='number of SGD steps during adaptation')
-    parser.add_argument("--meta_lr", default=0.001, type=float)
-
-    parser.add_argument("--train_flag", default=False, type=bool)
-    parser.add_argument("--minibatch_validation", default=32, type=int,
-                        help='completely irrelevant if we have no validation dataloader')
-    parser.add_argument("--num_epochs", default=500, type=int,
-                        help='number of training epochs. one epoch corresponds to one meta update for theta. model is stored all 500 epochs')
-    parser.add_argument("--minibatch", default=8, type=int,
-                        help='number of meta training tasks ')
-    parser.add_argument("--points_per_minibatch", default=16, type=int,
-                        help='number of datapoints in each meta training task')
-    parser.add_argument("--minbatch_test", default=128,
-                        type=int, help='number of meta testing tasks')
-    parser.add_argument("--points_per_minibatch_test", default=1024,
-                        type=int, help='number of datapoints in each meta testing task')
-    parser.add_argument("--minibatch_print", default=1, type=int,
-                        help='1 means training and validation loss are logged to wandb after each epoch')
-    parser.add_argument("--first_order", default=True, type=bool,
-                        help="Should always be true for MAML basd algos")
-
     parser.add_argument("--noise_stddev", default=0.02, type=float,
                         help='standard deviation of the white gaussian noise added to the data targets y')
     parser.add_argument("--seed", default=123, type=int,
@@ -61,22 +26,57 @@ def main():
                         help='data generation seed for the meta training tasks')
     parser.add_argument("--seed_offset_test", default=1234, type=int,
                         help='data generation seed for the meta testing task')
-    parser.add_argument("--normalize_benchmark", default=True, type=bool)
-    parser.add_argument("--benchmark", default='SinusoidAffine1D',
-                        help='possible values are Sinusoid1D, Affine1D, Quadratic1D, SinusoidAffine1D')
-    parser.add_argument("--num_visualization_tasks", default=4, type=int,
-                        help='number of randomly chosen meta testing tasks that are used for visualization')
-    parser.add_argument("--visualization_type", default="distribution", type=str,
-                        help="(distribution, samples) visualize via plotting a distribution or plotting the individual samples ")
-    parser.add_argument("--y_plotting_resolution", default=256, type=int,
-                        help="number of discrete y-axis points to evaluate for visualization")
+    parser.add_argument("--k_shot", default=8, type=int,
+                        help='number of datapoints in the context set (needs to be less than points_per_minibatch)')
+    parser.add_argument("--points_per_minibatch", default=16, type=int,
+                        help='number of datapoints in each meta training task (should be much less than points_per_minibatch_test)')
+    parser.add_argument("--minibatch", default=16, type=int,
+                        help='number of meta training tasks ')
+    parser.add_argument("--points_per_minibatch_test", default=1024, type=int,
+                        help='number of datapoints in each meta testing task')
+    parser.add_argument("--minbatch_test", default=32, type=int,
+                        help='number of meta testing tasks')
 
-    parser.add_argument("--wandb", default=False, type=bool,
-                        help="Specifies if logs should be written to WandB")
+    # algorithm parameter options
     parser.add_argument("--algorithm", default='maml',
                         help='possible values are maml, platipus, bmaml')
-    parser.add_argument("--epochs_to_store", default=500, type=int,
+    parser.add_argument("--network_architecture", default="FcNet")
+    parser.add_argument("--num_epochs", default=60000, type=int,
+                        help='number of training epochs. one epoch corresponds to one meta update for theta. model is stored all 500 epochs')
+    parser.add_argument("--epochs_to_store", default=5000, type=int,
                         help='number of epochs to wait until storing the model')
+    parser.add_argument("--num_models", default=10, type=int,
+                        help='number of models (phi) we sample from the posterior in the end for evaluation. irrelevant for maml')
+    parser.add_argument("--num_inner_updates", default=5, type=int,
+                        help='number of SGD steps during adaptation')
+    parser.add_argument("--inner_lr", default=0.01, type=float)
+    parser.add_argument("--meta_lr", default=0.001, type=float)
+    parser.add_argument("--KL_weight", default=1e-5, type=float)
+
+    # wandb options
+    parser.add_argument("--wandb", default=False, type=bool,
+                        help="Specifies if logs should be written to WandB")
+    parser.add_argument("--minibatch_print", default=1, type=int,
+                        help='1 means training and validation loss are logged to wandb after each epoch')
+    parser.add_argument("--num_visualization_tasks", default=4, type=int,
+                        help='number of randomly chosen meta testing tasks that are used for visualization')
+    parser.add_argument("--y_plotting_resolution", default=512, type=int,
+                        help="number of discrete y-axis points to evaluate for visualization")
+
+    # exotic cli options
+    parser.add_argument("--resume_epoch", default=0,
+                        help='0 means fresh training. >0 means training continues from a corresponding stored model.')
+    parser.add_argument("--logdir", default=".",
+                        help='default location to store the saved_models directory')
+    parser.add_argument("--minibatch_validation", default=32, type=int,
+                        help='completely irrelevant if we have no validation dataloader')
+    parser.add_argument("--first_order", default=True, type=bool,
+                        help="Should always be true for MAML basd algos")
+    parser.add_argument("--normalize_benchmark", default=True, type=bool)
+
+    # "I don't know what they do" options
+    parser.add_argument("--train_flag", default=False, type=bool)
+    parser.add_argument("--v_shot", default=10, type=int)
 
     args = parser.parse_args()
 
