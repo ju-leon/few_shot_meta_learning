ebfbb07f86cc8952a9df619279bb3b3e480a38c3
Leon Jungemeyer <leon.jungemeyer@me.com>
2022-01-11 12:48:46 +0100

    Train in parallel

--- start_job.sh
+++ start_job.sh
@@ -2,14 +2,13 @@
 #SBATCH --ntasks=1
 #SBATCH --partition=single
 #SBATCH --mem=16000
-#SBATCH --time=4:00:00
+#SBATCH --time=8:00:00
 #SBATCH --parsable
 
 echo 'Job started'
 
 EPOCHS=60000
-EPOCHS_TO_STORE=60001
-EPOCHS_TO_TEST=5000
+EPOCHS_TO_STORE=5000
 
 for ARGUMENT in "$@"
 do
@@ -18,19 +17,17 @@ do
     export "$KEY"=$VALUE
 done
 
-for num_samples in 1 2 4 8
+echo $BENCHMARK
+
+for num_samples in 1 2 4 8 1000
 do
-
-    for benchmark in Sinusoid1D Affine1D SinusoidAffine1D
-    do
-
         for seed in 1234 4321 9999
         do
 
 		python train.py --algorithm $ALGORITHM \
 		                --wandb True \
 		                --num_epochs $EPOCHS \
-		                --benchmark $benchmark \
+		                --benchmark $BENCHMARK \
 		                --num_models $NUM_MODELS \
 		                --k_shot $num_samples \
 		                --epochs_to_store $EPOCHS_TO_STORE \
@@ -39,5 +36,4 @@ do
 		                --seed_offset_test $seed \
 				--logdir_base /pfs/work7/workspace/scratch/utpqw-meta
 	done
-    done
 done
--- start_training.sh
+++ start_training.sh
@@ -3,10 +3,13 @@
 module load devel/cuda/11.0
 module load devel/cudnn/10.2
 
-for num_models in 5 100 1000
+for benchmark in Sinusoid1D Affine1D SinusoidAffine1D
 do
-    sbatch start_job.sh ALGORITHM="platipus" NUM_MODELS=$num_models
-    sbatch start_job.sh ALGORITHM="bmaml" NUM_MODELS=$num_models
-done
+	for num_models in 5 100 1000
+	do
+		sbatch start_job.sh ALGORITHM="platipus" NUM_MODELS=$num_models BENCHMARK=$benchmark
+		sbatch start_job.sh ALGORITHM="bmaml" NUM_MODELS=$num_models BENCHMARK=$benchmark
+	done
 
-sbatch start_job.sh ALGORITHM="maml" NUM_MODELS=1
+	sbatch start_job.sh ALGORITHM="maml" NUM_MODELS=1 BENCHMARK=$benchmark
+done
